65d01cabef1e4c4f129f289b467d6fa0a6a86ba9
ankan112 <ankanhalder112@gmail.com>
2024-01-11 22:52:13 +0600

    slice, substring done

--- String/app.js
+++ String/app.js
@@ -1,14 +1,26 @@
-// JavaScript String Length
+//! JavaScript String Length
 
 const sentence = "I like Cricket more than Football!";
-console.log(sentence.length);
+// console.log(sentence.length);
 
-// JavaScript charAt, charCodeAt, & at method
-console.log(sentence.charAt(11));
-console.log(sentence.charCodeAt(11));
-console.log(sentence.at(11));
+//! JavaScript charAt, charCodeAt, & at method
+// console.log(sentence.charAt(11));
+// console.log(sentence.charCodeAt(11));
+// console.log(sentence.at(11));
 /* 
 sting.charAt()
 string.chartCodeAt()
 string.at() // include in 2022 & support all the modern browsers
 */
+
+//! Extracting String Parts
+
+/* 
+substring() is similar to slice().
+The difference is that start and end values less than 0 are treated as 0 in substring().
+
+They work with length
+*/
+
+console.log(sentence.slice(4, 12));
+console.log(sentence.substring(4, 6));
