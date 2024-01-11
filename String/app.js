c41dd4bd29b16ac81d49b508c5ebd7c7bf408c0d
ankan112 <ankanhalder112@gmail.com>
2024-01-11 22:38:53 +0600

    string at method done

--- String/app.js
+++ String/app.js
@@ -2,3 +2,13 @@
 
 const sentence = "I like Cricket more than Football!";
 console.log(sentence.length);
+
+// JavaScript charAt, charCodeAt, & at method
+console.log(sentence.charAt(11));
+console.log(sentence.charCodeAt(11));
+console.log(sentence.at(11));
+/* 
+sting.charAt()
+string.chartCodeAt()
+string.at() // include in 2022 & support all the modern browsers
+*/
